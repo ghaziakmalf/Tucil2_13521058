f2ce04984ce2084a822ff95289184cecdf3ce75f
Ghazi Akmal Fauzan <13521058@std.stei.itb.ac.id>
2023-02-27 17:27:57 +0700

    Update sort function

--- src/utility.py
+++ src/utility.py
@@ -7,21 +7,15 @@ def distance(point1, point2):
     return math.sqrt(dis)
 
 def sort(points):
-    def quickSort(points, start, end):
-        if start < end:
-            pivot = partition(points, start, end)
-            quickSort(points, start, pivot - 1)
-            quickSort(points, pivot + 1, end)
-
-    def partition(points, start, end):
-        pivot = points[end][0]
-        i = start - 1
-        for j in range(start, end):
-            if points[j][0] <= pivot:
-                i += 1
-                points[i], points[j] = points[j], points[i]
-        points[i + 1], points[end] = points[end], points[i + 1]
-        return i + 1
-
-    quickSort(points, 0, len(points) - 1)
-    return points
+    if len(points) <= 1:
+        return points
+    else:
+        pivot = points[0]
+        less = []
+        greater = []
+        for i in range(1, len(points)):
+            if points[i] < pivot:
+                less.append(points[i])
+            else:
+                greater.append(points[i])
+        return sort(less) + [pivot] + sort(greater)
