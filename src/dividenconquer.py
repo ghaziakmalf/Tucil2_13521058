38021131d6d35cc434bc63c3c776240c9fb4236a
Ghazi Akmal Fauzan <13521058@std.stei.itb.ac.id>
2023-02-27 17:30:06 +0700

    Add calculation count + little fix (dnc)

--- src/dividenconquer.py
+++ src/dividenconquer.py
@@ -1,27 +1,28 @@
 from utility import distance
 
-def dividenconquer(points):
+def dividenconquer(points, nCalculation = 0):
     if len(points) == 2:
-        return distance(points[0], points[1]), points[0], points[1]
+        nCalculation += 1
+        return distance(points[0], points[1]), points[0], points[1], nCalculation
     
     elif len(points) == 3:
         minDistance = distance(points[0], points[1])
         point1 = points[0]
         point2 = points[1]
-        # Brute Force
         for i in range(len(points)):
             for j in range(i+1, len(points)):
                 dis = distance(points[i], points[j])
+                nCalculation += 1
                 if dis < minDistance:
                     minDistance = dis
                     point1 = points[i]
                     point2 = points[j]
-        return minDistance, point1, point2
+        return minDistance, point1, point2, nCalculation
     
     else:
         mid = len(points) // 2
-        leftMinDistance, leftPoint1, leftPoint2 = dividenconquer(points[:mid])
-        rightMinDistance, rightPoint1, rightPoint2 = dividenconquer(points[mid:])
+        leftMinDistance, leftPoint1, leftPoint2, nCalculation = dividenconquer(points[:mid], nCalculation)
+        rightMinDistance, rightPoint1, rightPoint2, nCalculation = dividenconquer(points[mid:], nCalculation)
         if leftMinDistance < rightMinDistance:
             minDistance = leftMinDistance
             point1 = leftPoint1
@@ -31,19 +32,25 @@ def dividenconquer(points):
             point1 = rightPoint1
             point2 = rightPoint2
 
-        midX = points[mid][0]
-        minPoints = []
-        for point in points:
-            if abs(point[0] - midX) < minDistance:
-                minPoints.append(point)
+        midPoint = points[mid][0]
+        midPoints = []
+        for i in range(len(points)):
+            if (midPoint - minDistance) < points[i][0] < (midPoint + minDistance):
+                midPoints.append(points[i])
 
-        # Brute Force
-        for i in range(len(minPoints)):
-            for j in range(i+1, len(minPoints)):
-                dis = distance(minPoints[i], minPoints[j])
-                if dis < minDistance:
-                    minDistance = dis
-                    point1 = minPoints[i]
-                    point2 = minPoints[j]
-        
-        return minDistance, point1, point2
+        for i in range(len(midPoints)):
+            for j in range(i+1, len(midPoints)):
+                calculate = True
+                for k in range(len(midPoints[i])):
+                    if abs(midPoints[i][k] - midPoints[j][k]) > minDistance:
+                        calculate = False
+                        break
+                if calculate:
+                    dis = distance(midPoints[i], midPoints[j])
+                    nCalculation += 1
+                    if dis < minDistance:
+                        minDistance = dis
+                        point1 = midPoints[i]
+                        point2 = midPoints[j]
+
+        return minDistance, point1, point2, nCalculation
