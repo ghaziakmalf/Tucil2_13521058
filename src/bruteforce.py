4da9c8a1538be2e0cd04bafa7b1e4d6065a2031f
Ghazi Akmal Fauzan <13521058@std.stei.itb.ac.id>
2023-02-27 17:29:30 +0700

    Add calculation count (bf)

--- src/bruteforce.py
+++ src/bruteforce.py
@@ -4,11 +4,13 @@ def bruteforce(points):
     minDistance = distance(points[0], points[1])
     point1 = points[0]
     point2 = points[1]
+    nCalculation = 0
     for i in range(len(points)):
         for j in range(i+1, len(points)):
             dis = distance(points[i], points[j])
+            nCalculation += 1
             if dis < minDistance:
                 minDistance = dis
                 point1 = points[i]
                 point2 = points[j]
-    return minDistance, point1, point2
+    return minDistance, point1, point2, nCalculation
